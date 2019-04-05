cce9379525db3e0a46baa8da17bf141ab7da315a
Jchapmn <47482165+Jchapmn@users.noreply.github.com>
2019-04-05 09:19:07 -0500

    Add files via upload

---
+++ FinalProject.py
@@ -0,0 +1,13 @@
+'''
+Created on Apr 5, 2019
+
+@author: ITAUser
+OBJECTIVE:
+Making a stress reliever that helps out to reduce stress, and give opinions on how to solve stress
+ALGORTHM:
+1) Ask the person his/her name
+2) Ask them from 1-10 how they will rate their stress
+3) Ask them multiple choice questions
+4) Make a way to give them opinions on how to solve their stress based on their answers.
+
+'''
